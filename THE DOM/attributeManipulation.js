cf6b23f5f8ae41bac90650b23513387de46aa8b5
janiraina29-gif <janiraina29@gmail.com>
2026-08-10 23:22:36 +0530

    Added attribute manipulation

---
+++ THE DOM/attributeManipulation.js
@@ -0,0 +1,12 @@
+//SET ATTRIBUTE 
+let a = document.querySelector("a");
+a.setAttribute("href", "https://www.google.com");
+
+//GET ATTRIBUTE
+//let a=document.querySelector("a");
+console.dir(a.getAttribute("href"));
+//gets the href attribute from the a tag and prints it in console.
+
+//REMOVE ATTRIBUTE
+//a.removeAttribute("href");
+//this removes the value of href attribute from the a tag.
