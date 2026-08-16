600d6b858b416b5ced19b8843e2029c8f761e285
janiraina29-gif <janiraina29@gmail.com>
2026-08-16 16:38:37 +0530

    TASK 3 and 4 updated

---
+++ THE DOM/TASKS/TASK3.js
@@ -0,0 +1,4 @@
+//select the text of a paragraph and replace it with :
+// <b> UPDATED </b> by javascript
+let p= document.querySelector("p");
+p.innerHTML="<b>UPDATED</b>"
---
+++ THE DOM/TASKS/TASK4.js
@@ -0,0 +1,3 @@
+//adding a title attribute to a div dynamically.
+let div=document.querySelector("div");
+div.setAttribute("title",  "some information");
